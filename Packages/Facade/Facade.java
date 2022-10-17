879fc2ca9ff39da06c3008e25e107ede6f9e8dbb
Varun Menon <53273234+VarunMenon-ux@users.noreply.github.com>
2022-10-16 18:21:32 -0700

    Add files via upload

---
+++ Packages/Facade/Facade.java
@@ -0,0 +1,84 @@
+package Facade;
+
+public class Facade {
+
+	private int UserType;
+
+	private Product theSelectedProduct;
+
+	private int nProductCategory;
+
+	private ClassProductList theProductList;
+
+	private Person thePerson;
+
+	public boolean login() {
+		return false;
+	}
+
+	public void addTrading() {
+
+	}
+
+	public void viewTrading() {
+
+	}
+
+	/**
+	 *  
+	 */
+	public void viewOffering() {
+
+	}
+
+	/**
+	 *  
+	 */
+	public void markOffering() {
+
+	}
+
+	/**
+	 *  
+	 */
+	public void submitOffering() {
+
+	}
+
+	/**
+	 *  
+	 */
+	public void remind() {
+
+	}
+
+	public void createUser(UserInfoItem userinfoitem) {
+
+	}
+
+	/**
+	 *  
+	 */
+	public void createProductList() {
+
+	}
+
+	/**
+	 *  
+	 */
+	public void AttachProductToUser() {
+
+	}
+
+	public Product selectProduct() {
+		return null;
+	}
+
+	/**
+	 *  
+	 */
+	public void productOperation() {
+
+	}
+
+}
